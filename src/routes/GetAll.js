3e672314f10c8c61a358c509ded477db2935e6b4
Hugo Valencia <hvb1507@hotmail.com>
2023-07-17 22:25:10 -0600

    update

---
+++ src/routes/GetAll.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const router = express.Router();
+const  getAll  = require('../controllers/getAll');
+
+router.get("/", getAll);
+
+module.exports = router;
+
+
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 const { Router } = require('express');
 
 // Import all the routers;
+// const exampleRouter = require('./example.js');
+
+const  getAll  = require('./GetAll.js');
+
 
 const router = Router();
 
 // Configure router;
 
 // router.use('/example', exampleRouter);
+
+router.use(getAll);
+
 
 module.exports = router;
