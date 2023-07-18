a580905fa8143ee19e018bd93e11f6bf386c0357
Hugo Valencia <hvb1507@hotmail.com>
2023-07-17 23:54:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { PORT } = process.env;
 // hjlkhlkklkjkjjjjññlkñkñkñ
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-    server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    server.listen(3001, () => {
+    console.log(`%s listening at ${3001}`); // eslint-disable-line no-console
     });
 }
 );
