d95aaedfb82917bf55d07753f0c261620b9c7d75
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-07-18 22:54:17 -0400

    back 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 
-const { agregaLibro } = require('../controllers/agregaLibro');
+const { agregaLibro } = require('../controllers/agregaLibro.js');
+
+const { obtenerLibros } = require('../controllers/obtenerLibros.js');
 
 const router = Router();
 
 
 router.post('/agregaLibro',agregaLibro );
+router.get('/obtenerLibros', obtenerLibros);
 
 router.get('/', (req, res) => { res.send('¡Bienvenido a la API!');});
 
